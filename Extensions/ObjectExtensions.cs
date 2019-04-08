85dfea417931f583f812f1c2358b317b616c39b1
Logan Laughlin <llaughlin@penlink.com>
2019-04-08 10:06:08 -0500

    Added struct version of `With`

--- Extensions/ObjectExtensions.cs
+++ Extensions/ObjectExtensions.cs
@@ -10,7 +10,12 @@ namespace Extensions
             return JsonConvert.SerializeObject(source);
         }
 
-        public static T With<T>(this T source, Action<T> accessor)
+        public static T With<T>(this T source, Action<T> accessor) where T : class
+        {
+            accessor(source);
+            return source;
+        }
+        public static T With<T>(this ref T source, Action<T> accessor) where T : struct
         {
             accessor(source);
             return source;
